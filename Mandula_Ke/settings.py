b8794ae437d168bf4a3e725488b7953e589a0493
gacharahenry8-design <gacharahenry8@gmail.com>
2026-05-26 23:25:29 +0300

    Fix ALLOWED_HOSTS

--- Mandula_Ke/settings.py
+++ Mandula_Ke/settings.py
@@ -12,26 +12,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-ga+7k_wyaah9-467+p7yoex#bc!zt=#le&3a6gh5-^x8y*i!lb"
+)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ga+7k_wyaah9-467+p7yoex#bc!zt=#le&3a6gh5-^x8y*i!lb'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-import os
-
-ALLOWED_HOSTS = [
-    "mandula-ke.onrender.com",
-    "127.0.0.1",
-    "localhost"
-]
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "mandula-ke.onrender.com,127.0.0.1,localhost"
+).split(",")
 
 # Application definition
 
